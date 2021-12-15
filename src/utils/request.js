1b4c51ec61016fab4333710416e71d2cdca93637
Coding-Bug <accjf@outlook.com>
2021-12-15 22:09:49 +0800

    1

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,7 +5,7 @@ import router from '@/router'
 
 // create an axios instance
 const service = axios.create({
-  baseURL: 'http://ipandai.club/', // 在服务器上用这个
+  baseURL: 'https://ipandai.club/', // 在服务器上用这个
   // baseURL: 'http://127.0.0.1:4523/mock/401326/',
   timeout: 5000, // request timeout
   withCredentials: true
