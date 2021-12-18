24c769a74a69b31b338190d10d4e51b6d8d1e5c1
Coding-Bug <accjf@outlook.com>
2021-12-18 09:55:34 +0800

    cjftext

--- src/permission.js
+++ src/permission.js
@@ -5,28 +5,28 @@ import 'nprogress/nprogress.css' // progress bar style
 
 NProgress.configure({ showSpinner: false }) // NProgress Configuration
 
-// router.beforeEach(async(to, from, next) => {
-//   // 加载进度条
-//   NProgress.start()
-//   if (store.getters.name === '' || store.getters.name == null || store.getters.name === undefined) {
-//     await store.dispatch('user/getUserInfo')
-//   }
-//   if (to.path === '/login') {
-//     if (store.getters.name) {
-//       next({ path: '/table' })
-//     } else {
-//       next()
-//     }
-//   } else {
-//     if (store.getters.name) {
-//       next()
-//     } else {
-//       next({ path: '/login' })
-//     }
-//   }
-// })
+router.beforeEach(async(to, from, next) => {
+  // 加载进度条
+  NProgress.start()
+  if (store.getters.name === '' || store.getters.name == null || store.getters.name === undefined) {
+    await store.dispatch('user/getUserInfo')
+  }
+  if (to.path === '/login') {
+    if (store.getters.name) {
+      next({ path: '/table' })
+    } else {
+      next()
+    }
+  } else {
+    if (store.getters.name) {
+      next()
+    } else {
+      next({ path: '/login' })
+    }
+  }
+})
 
-// router.afterEach(() => {
-//   // finish progress bar
-//   NProgress.done()
-// })
+router.afterEach(() => {
+  // finish progress bar
+  NProgress.done()
+})
